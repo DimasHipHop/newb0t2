f964e694e904a49865e62b231a2d0b5abbdd591c
DimasHipHop <32538352+DimasHipHop@users.noreply.github.com>
2019-03-20 19:04:17 +0500

    Add files via upload

---
+++ Ping.cs
@@ -0,0 +1,78 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShikimoriNET;
+using ShikimoriNET.Categories;
+using ShikimoriNET.Categories.Interfaces;
+using ShikimoriNET.Params.Anime;
+using ShikimoriNET.Enums.Anime;
+using ShikimoriNET.Enums;
+
+
+namespace newb0t
+{
+   public class Ping : ModuleBase<SocketCommandContext>
+    {
+
+        private  ShikimoriApi _api;
+
+
+
+
+        [Command("anime")]
+        public async Task PingAsync()
+        {
+
+
+            _api = new ShikimoriApi();
+            var animes = await _api.Anime.SearchAsync(new SearchParams
+            {
+               
+                Order = Order.Random,
+                Censored = false,
+                Score = 7,
+                Limit = 100000
+              
+            });
+
+            Random r = new Random();
+
+            var a = animes.ElementAt(r.Next(0, animes.Count()));
+         
+
+            await ReplyAsync(a.Name+"|"+ a.Russian + "\n https://shikimori.org" + a.Url);
+          
+
+        }
+
+      
+
+        [Command("ban"), RequireUserPermission(Discord.GuildPermission.ManageRoles)]
+        public async Task Ban(IGuildUser user)
+        {
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "banned");
+
+            
+            await (user as IGuildUser).AddRoleAsync(role);
+            await ReplyAsync($"{user.Mention} был забанен, видимо есть за что..." );
+
+        }
+
+        [Command("unban"), RequireUserPermission(Discord.GuildPermission.ManageRoles)]
+        public async Task unBan(IGuildUser user)
+        {
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "banned");
+
+
+            await (user as IGuildUser).RemoveRoleAsync(role);
+            await ReplyAsync($"{user.Mention} Разбанен!");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace newb0t
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = "NTU3MTI5OTM4Mzg2NTUwNzk0.D3JqLg.bPxltMahCM0iINl9CubKYLSiAVo";
+
+
+            _client.Log += Log;
+            _client.UserJoined += Joined;
+          await  _client.SetGameAsync("Заходи!", "https://twitch.tv/newb0dy", ActivityType.Streaming);
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(Discord.TokenType.Bot, botToken);
+            await _client.StartAsync();
+
+         
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Joined(SocketGuildUser user)
+        {
+            var guild = user.Guild;
+          //  var channel = guild.DefaultChannel;
+            var channel = guild.GetTextChannel(547080441971212330);
+      
+            Random r = new Random();
+            switch (r.Next(1, 5))
+            {
+                case 1 :  await channel.SendMessageAsync($"Добро пожаловать, {user.Mention}");break;
+                case 2: await channel.SendMessageAsync($"Все приветствуем {user.Mention}!"); break;
+                case 3: await channel.SendMessageAsync($"{user.Mention} теперь один из нас!"); break;
+                case 4: await channel.SendMessageAsync($"лол, кек, {user.Mention}"); break;
+                case 5: await channel.SendMessageAsync($"А вот и ты! {user.Mention}!"); break;
+            }
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(),_services);
+
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("0!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+               var result =  await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
